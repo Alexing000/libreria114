32c4bedd24f2e13fd96ee1e4512f6df3c624bebf
Antonino Giuseppe Lucarello <Lucarelloantoninogiuseppe01@gmail.com>
2024-05-16 15:23:50 +0200

    Create LibroService.java

---
+++ Progetto/src/main/java/com/example/Progetto/services/LibroService.java
@@ -0,0 +1,32 @@
+package com.example.Progetto.services;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.example.Progetto.dao.DaoLibro;
+import com.example.Progetto.models.Libro;
+
+@Service
+public class LibroService extends GenericService <Long, Libro, DaoLibro>{
+
+    @Override
+    public Libro createEntity(Map<String, String> map) {
+        Libro l = getContext().getBean(Libro.class, map);
+        return l;
+    }
+
+    public Libro insert(Map<String, String> map){
+        Libro l = createEntity(map);
+        Long id = getDao().create(l);
+        l.setId(id);
+        return l;
+    }
+    
+    public List<Libro> findAll() {
+        return getDao().read().values().stream().map(x -> {
+            return (Libro) x;
+        }).toList();
+    }
+}
