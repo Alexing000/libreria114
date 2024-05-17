917f34fcdd6e7bee2a75d8060cd14a053df90b5c
DoiniP <167433286+DoiniP@users.noreply.github.com>
2024-05-17 16:41:41 +0200

    Update AutoreController.java

--- Progetto/src/main/java/com/example/Progetto/controllers/AutoreController.java
+++ Progetto/src/main/java/com/example/Progetto/controllers/AutoreController.java
@@ -5,20 +5,21 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.example.Progetto.models.Autore;
 import com.example.Progetto.services.ServiceAutore;
 
 import lombok.Data;
 
-@RestController
+@Controller
 @Data
 @RequestMapping("/api/autore")
 public class AutoreController {
